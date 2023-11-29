7aced44b8ddc388b511ee11ffff7fd5ce92dcf0e
Frank Rodriguez <frank.rodriguez.dev@gmail.com>
2023-11-29 15:26:45 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,30 @@
 
 //
 $(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrolling': false,
-				'autoScrolling': false,
-				'css3': true,
-				'navigation': true,
-				'navigationPosition': 'right',
-			});
-		});
+  // Initialize fullPage.js
+  $('#fullpage').fullpage({
+    'verticalCentered': false,
+    'scrolling': false,
+    'autoScrolling': false,
+    'css3': true,
+    'navigation': true,
+    'navigationPosition': 'right',
+  });
 
-// wow
-$(function()
-{
-    new WOW().init();
-    $(".rotate").textrotator();
-    $('.fp-viewing').removeClass('fp-viewing');
-})
+  // Add a delay to the auto-scrolling between sections
+  function delayScroll() {
+    // Replace 2000 with the desired delay in milliseconds (e.g., 2000 for 2 seconds)
+    setTimeout(function() {
+      $.fn.fullpage.moveSectionDown();
+      delayScroll(); // Recursive call for continuous scrolling
+    }, 2000);
+  }
+
+  // Start the delayed scrolling when the document is ready
+  delayScroll();
+
+  // Initialize WOW.js and other custom functions
+  new WOW().init();
+  $(".rotate").textrotator();
+  $('.fp-viewing').removeClass('fp-viewing');
+});
